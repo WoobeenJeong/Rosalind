133b7458541c8a58efc4284ea475ea1dd36264d1
Woobeen Jeong <jwb419@snu.ac.kr>
2023-12-19 15:16:40 +0900

    Update ba1a.py
    naive exact pattern match

--- BA1/ba1a.py
+++ BA1/ba1a.py
@@ -1,17 +1,16 @@
 
-
-with open("bioinfo2/rosalind_ba1a.txt", "r") as file:
-    Text = file.readline().strip()
-    Pattern = file.readline().strip() 
-    
-# Text, Pattern
-
 def counting(Text, Pattern):
     count = 0
     for i in range(len(Text) - len(Pattern) + 1):
         if Text[i:i+len(Pattern)] == Pattern:
             count += 1
     return count
+
+with open("bioinfo2/rosalind_ba1a.txt", "r") as file:
+    Text = file.readline().strip()
+    Pattern = file.readline().strip() 
+    
+# print(Text, Pattern)
 
 result = counting(Text, Pattern)
 print(result)
