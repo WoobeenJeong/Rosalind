fad823066d8089b13b13aebbec30d70388a0ef87
Woobeen Jeong <jwb419@snu.ac.kr>
2023-12-19 16:08:46 +0900

    brute force pattern match

--- BA1/ba1d.py
+++ BA1/ba1d.py
@@ -3,7 +3,7 @@
 
 def brute_force(pattern, string):
     positions = []
-    for i in range(len(string) - len(pattern) + 1):    ## 범위 중요
+    for i in range(len(string) - len(pattern) + 1):    # take care of range
         if string[i:i+len(pattern)] == pattern:
             positions.append(i)
     return positions
