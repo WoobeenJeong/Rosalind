e51a18f814bd465ab2253d8783a1efc144a92be4
Woobeen Jeong <jwb419@snu.ac.kr>
2023-12-21 17:17:57 +0900

    hamming distance

--- TextBook/BA1/ba1g.py
+++ TextBook/BA1/ba1g.py
@@ -1,7 +1,6 @@
-
 ### Hamming distance : X,Y 두 개의 string 존재 시, |X|=|Y|로 길이 같을 때, X를 Y로 바꾸기 위한 최소 개수 = 다른 글자 개수 (XOR개념)
 
-# def XOR(string01, string02):
+# def hamming(string01, string02):
 def hamming(string01, string02):
     if len(string01) != len(string02):
         print("Error:Using Edit distance")
