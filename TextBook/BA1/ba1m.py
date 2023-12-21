868d40170d64bb4b3c6cf18127438e5574bdb36c
Woobeen Jeong <jwb419@snu.ac.kr>
2023-12-21 17:24:10 +0900

    number to pattern

--- TextBook/BA1/ba1m.py
+++ TextBook/BA1/ba1m.py
@@ -1,4 +1,3 @@
-
 ### Number to Pattern
 ### A,C,G,T를 각각 0,1,2,3으로 할당
 ### 이때, 자리수 기준으로 곱 적용
@@ -6,15 +5,13 @@
 ### ba1l.py의 역함수
 
 def pattern_to_number(number, kmer):
-        
         nt = {"A":0,"C":1,"G":2,"T":3}
         pattern = ""
         
         for i in range(kmer):
-            
             pattern += list(nt.keys())[list(nt.values()).index(number % 4)]
             number = number // 4
-        
+                
         pattern = "".join(reversed(pattern))
 
         return pattern
