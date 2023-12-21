fd4422a263fcdaa47b4ede0018195eb26656a0fa
Woobeen Jeong <jwb419@snu.ac.kr>
2023-12-21 17:23:00 +0900

    pattern to number as quaternary system

--- TextBook/BA1/ba1l.py
+++ TextBook/BA1/ba1l.py
@@ -1,11 +1,8 @@
-
 ### Pattern to Number
 ### 1. A,C,G,T를 각각 0,1,2,3으로 할당
 ### 이때, 자리수 기준으로 곱 적용
 
-
 def pattern_to_number(pattern):
-        
         nt = {"A":0,"C":1,"G":2,"T":3}
         k = len(pattern)
         num = 0
@@ -15,14 +12,13 @@ def pattern_to_number(pattern):
 
         return num
 
-
 with open('bioinfo2/rosalind_ba1l.txt') as f:
     pattern = f.readline().strip() 
     
 print(pattern)
 print(pattern_to_number(pattern))
-      
-# print(pattern_to_number("AAC")) # 0 + 0 + 1
-# print(pattern_to_number("ACA")) # 0 + 4 + 0
-# print(pattern_to_number("CAA")) # 16 + 0 + 0
 
+### examples
+### print(pattern_to_number("AAC")) # 0 + 0 + 1
+### print(pattern_to_number("ACA")) # 0 + 4 + 0
+### print(pattern_to_number("CAA")) # 16 + 0 + 0
