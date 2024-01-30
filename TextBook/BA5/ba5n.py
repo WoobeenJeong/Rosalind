9fbb52b3c6b9d3bb3a544ef1929d71468ff1aff9
Woobeen Jeong <jwb419@snu.ac.kr>
2024-01-30 14:25:38 +0900

    DAG topological order

--- TextBook/BA5/ba5n.py
+++ TextBook/BA5/ba5n.py
@@ -1,3 +1,5 @@
+### topological ordering으로 root를 가져오기
+### 이번엔 일단, path정보를 불러와서 path에 대한 정보를 before -> after로 root형태로 [ ] tuple 저장
 
 import numpy as np
 
@@ -25,40 +27,40 @@ def topo_order(root):
     # print(matrix)
     
     indegree = np.zeros(nodes)                                      # indegree = edge받는 end node의 연결 갯수
-    queue = []                                                      # queue = 임시저장
-    T = []                                                          # T = 결과값
+    memory = []                                                      # memory = 임시저장
+    result = []                                                     # result = 결과값
     
     for edge in root:                                               # 2. 모든 node에 대해, end node가 될 때, edge의 연결 개수 저장 
         start, end = edge
         indegree[end] += 1
     # print(indegree)
     
-    for i in range(nodes):                                          # 3. indegree가 0인 node를 queue에 저장 (시작점)
+    for i in range(nodes):                                          # 3. indegree가 0인 node를 memory에 저장 (=시작점 찾기)
         if indegree[i] == 0:
-            queue.append(i)
-    # print(queue)
+            memory.append(i)
+    # print(memory)
         
-    while queue:
-        node = queue.pop(0)                                         # 4. queue에서 node를 하나씩 빼서 T에 저장 (즉, 연결 개수 작은 순서로 T에 저장)
-        T.append(node)
+    while memory:
+        node = memory.pop(0)                                         # 4. memory에서 node를 하나씩 빼서 T에 저장 (즉, 연결 개수 작은 순서로 T에 저장)
+        result.append(node)
             
         for i in range(nodes):
             if matrix[node][i] != 0:
-                indegree[i] -= 1                                    # 연결개수가 언젠가는 0이 되고, 그때마다 queue에 저장, 그걸 맨 위의 while문에서 T에 저장 반복
+                indegree[i] -= 1                                       # 연결개수가 언젠가는 0이 되고 -> memory에 저장, 그걸 맨 위의 while문에서 result에 저장 반복
                 
                 if indegree[i] == 0:
-                    queue.append(i)                    
-        # print(T)
+                    memory.append(i)                    
+        # print(result)
     
     if any(0 in pair for pair in root):
-        result = T
+        final = result
     else:
-        result = []                                                 # T에서 0번째 node (사실상 존재X) 제외 (만약 0이라는 node가 없으면)
-        for x in T:
+        final = []                                                 # result에서 0번째 node (사실상 존재X) 제외 (만약 0이라는 node가 없으면)
+        for x in result:
             if x != 0:
-                result.append(x)
+                final.append(x)
     
-    return result
+    return final
 
 answer = ', '.join(map(str, topo_order(root)))                      # 그냥 print(topo_order(root))하면 list로 출력
 print(answer)                                                       # 이렇게 해야 [ , , ]이 아닌, , , 로 출력
