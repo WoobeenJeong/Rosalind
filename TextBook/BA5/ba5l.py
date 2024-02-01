6fe6ef1d070ff12795f2505c34465edac8267080
Woobeen Jeong <jwb419@snu.ac.kr>
2024-02-01 11:51:34 +0900

    middle edge align : divide & conquer

--- TextBook/BA5/ba5l.py
+++ TextBook/BA5/ba5l.py
@@ -1,90 +1,119 @@
+### 이번엔 middle edge 찾기를 반복해서 divide and conquer
+### 전체 local alignment를 찾기
+### 기존 ba5k 활용
 
-
-###############################################################################################
-### 이번엔 단순하게가 아니라 Middle egde와 Middle node를 통해 align하기
-
-
+import time
 import numpy as np
 
-def linear_space_alignment(vstr, wstr, sigma):
-    n = len(vstr)
-    m = len(wstr)
-
-    upper = np.zeros(m + 1, dtype=int)
-    lower = np.zeros(m + 1, dtype=int)
-    middle_edge = np.empty((n, m), dtype=str)
-
-    for j in range(1, m + 1):
-        upper[j] = upper[j - 1] - sigma
-        # print(upper)
-
-    for i in range(1, n + 1):
-        lower[0] = upper[0] - sigma
-        # print(lower)
-
-        for j in range(1, m + 1):
-            match = upper[j - 1] + BLOSUM62[vstr[i - 1]][wstr[j - 1]]
-            delete = upper[j] - sigma
-            insert = lower[j - 1] - sigma
-
-            upper[j - 1] = max(match, delete, insert)
-
-            lower[j] = max(match, upper[j] - sigma, lower[j - 1] - sigma)
-
-            if upper[j - 1] >= delete and upper[j - 1] >= insert:
-                middle_edge[i - 1, j - 1] = 'D'
-            elif delete >= insert:
-                middle_edge[i - 1, j - 1] = 'U'
-            else:
-                middle_edge[i - 1, j - 1] = 'L'
-
-        upper, lower = lower, upper
-
-    # print(f"{upper}\n{lower}\n{middle_edge}")
+with open('bioinfo2/BLOSUM62.txt', 'r') as file:
+    lines = file.readlines()
+    blosum62 = []
+    aa = lines[0].strip().split()[0:]
+    for line in lines[1:]:
+        row = [int(x) for x in line.strip().split()[1:]]
+        blosum62.append(row)
     
-    i, j = n - 1, m - 1
-    v_match, w_match = [], []
+def scoring(ref, alt, indel):
+    before = np.zeros((len(ref) + 1), dtype=np.int64)
+    after = np.arange(0, -indel * (len(ref) + 1), -indel, dtype=np.int64)
+    backtrack = np.zeros((len(ref) + 1), dtype=np.int8)
 
-    while i >= 0 or j >= 0:
-        if i >= 0 and j >= 0:
-            if middle_edge[i, j] == 'D':
-                v_match.append(vstr[i])
-                w_match.append(wstr[j])
-                i -= 1
-                j -= 1
-            elif middle_edge[i, j] == 'U':
-                v_match.append(vstr[i])
-                w_match.append('-')
-                i -= 1
+    for i, inext in enumerate(alt, 1):                                          
+        before, after = after, before
+        after[0] = -indel * i
+        for j, jnext in enumerate(ref, 1):
+            match = before[j - 1] + blosum62[aa.index(jnext)][aa.index(inext)]
+            insertion = after[j - 1] - indel
+            deletion = before[j] - indel
+            after[j] = max(match, insertion, deletion)
+
+            if after[j] == match:
+                backtrack[j] = 2
+            elif after[j] == insertion:
+                backtrack[j] = 1
             else:
-                v_match.append('-')
-                w_match.append(wstr[j])
-                j -= 1
-        elif i >= 0:
-            v_match.append(vstr[i])
-            w_match.append('-')
-            i -= 1
+                backtrack[j] = 0
+
+    return after, backtrack
+
+def middle_edge(ref, alt, indel):
+    midpoint = len(alt) // 2
+    
+    source2m, _ = scoring(ref, alt[:midpoint], indel)
+    m2sink, traceback = scoring(ref[::-1], alt[midpoint:][::-1], indel)
+    total = source2m + m2sink[::-1]
+
+    i = np.argmax(total)
+    j = midpoint
+    
+    addscore = traceback[::-1][i]
+    
+    if addscore == 0:
+        k = i
+        l = j + 1
+    elif addscore == 1:
+        k = i + 1
+        l = j
+    elif addscore == 2:
+        k = i + 1
+        l = j + 1
+
+    return (i, j), (k, l)
+
+############################################################################
+
+def alignment(top, bottom, left, right, ref, alt, indel):
+    if left == right:
+        return [(i, left) for i in range(top, bottom + 1)]
+    if top == bottom:
+        return [(top, j) for j in range(left, right + 1)]
+
+    partial_ref = ref[top:bottom]
+    partial_alt = alt[left:right]
+    (mi, mj), (mk, ml) = middle_edge(partial_ref, partial_alt, indel)
+    mi += top; mk += top
+    mj += left; ml += left
+    # print(partial_ref, partial_alt, mi, mj, mk, ml)
+    
+    source2mid = alignment(top, mi, left, mj, ref, alt, indel)
+    mid2sink = alignment(mk, bottom, ml, right, ref, alt,indel)
+    # print(source2mid, mid2sink)
+    
+    return source2mid + mid2sink
+
+def traceback(ref, alt, path, indel):
+    alignref = []
+    alignalt = []
+    score = 0
+    for (i, j), (k, l) in zip(path, path[1:]):
+        alignref.append(ref[i] if i < k else '-')
+        alignalt.append(alt[j] if j < l else '-')
+        if i < k and j < l:
+            score += + blosum62[aa.index(ref[i])][aa.index(alt[j])]
         else:
-            v_match.append('-')
-            w_match.append(wstr[j])
-            j -= 1
+            score -= indel
 
-    return upper[m], ''.join(v_match[::-1]), ''.join(w_match[::-1])
+    return score, ''.join(alignref), ''.join(alignalt)
 
-with open ("bioinfo2/rosalind_ba5l.txt", "r") as f:
-    seqs = f.readlines()
-    vstr = seqs[0].strip()
-    wstr = seqs[1].strip()
+############################################################################
+### 문제는 서열이 매우 길어서 오래 걸림
 
-with open ("bioinfo2/BLOSUM62.txt", "r") as f:
-    BLOSUM62 = f.readlines()
-    BLOSUM62 = [line.strip().split() for line in BLOSUM62]
-    BLOSUM62 = {line[0]:{BLOSUM62[0][i]:int(line[i+1]) for i in range(len(BLOSUM62[0]))} for line in BLOSUM62[1:]}
+with open('bioinfo2/rosalind_ba5l.txt', 'r') as file:
+    lines = file.readlines()
+    ref = lines[0].strip().split()[0]
+    alt = lines[1].strip().split()[0]
+    
+    indel = 5
 
-# print(vstr, wstr)
-# print(BLOSUM62)
+start_time = time.time()
 
-score, v_match, w_match = linear_space_alignment(vstr, wstr, sigma=5)
-print(score)
-print(v_match)
-print(w_match)
+path = alignment(0, len(ref), 0, len(alt), ref, alt, 5)
+score, alignref, alignalt = traceback(ref, alt, path, indel)
+
+end_time = time.time()
+elapsed_time = (end_time - start_time)/60
+
+print(score, alignref, alignalt, sep='\n')
+print(f"Elapsed Time: {round(elapsed_time,3)} min")
+
+### Elapsed Time: 5.797 min
